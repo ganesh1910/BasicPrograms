586068be313e4f86f1e6f9826ea016bd0f1fa8aa
Ganesh Khandagale <68702026+ganesh1910@users.noreply.github.com>
2023-09-11 22:28:17 +0530

    Test1

--- Koltin/src/main/java/com/gk/koltin/RemoveFirstLastCharacter.kt
+++ Koltin/src/main/java/com/gk/koltin/RemoveFirstLastCharacter.kt
@@ -1,9 +1,25 @@
 package com.gk.koltin
 
 fun main() {
-    println("welcome".removeUsingDrop())
+    "Hello World".removeUsingSubString()
+    "Hello World".removeUsingDrop()
+    "Hello World".removeUsingFor()
 }
 
-fun String.removeUsingDrop() {
-    println(this.drop(0).dropLast(this.length - 1))
+fun String.removeUsingSubString() {
+    println(this.substring(1,this.length - 1))
+}
+
+fun String.removeUsingDrop(){
+    println(this.drop(1).dropLast(1  ))
+}
+
+fun String.removeUsingFor(){
+    var temp=""
+    for (i in indices){
+        if(i!=0 && i!=this.length-1){
+               temp+=this[i]
+        }
+    }
+    println(temp)
 }
